ac879d654fdc5bbbd8822bf3a48ddc08a958e1a7
Jake Lumetta <lumetta@gmail.com>
2020-05-26 09:57:01 -0400

    Setting defaults per feedback

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ app.register_blueprint(blog, url_prefix='/blog')
 @app.route('/')
 def hello_world():
     return 'Homepage of our sample app. Update YOUR_API_TOKEN in blog_blueprint.py then go to /blog'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
